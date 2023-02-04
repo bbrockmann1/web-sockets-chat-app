dc71ac02cc25198b392780a01f22f3b200ed6be6
Bo Brockmann <bbrockmann1@gmail.com>
2023-02-04 10:37:46 -0700

    added components to app.js

--- chat-app/client/src/App.js
+++ chat-app/client/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import ChatroomList from './components/chatroomList';
 import ChatWindow from './components/chatWindow';
+import UsernameChatroom from './components/usernameChatroom';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="appBackground">
-      
+      <Header />
       
       <ChatWindow /> 
+      <ChatroomList />
+      <UsernameChatroom />
+
+      <Footer /> 
     </div>
   );
 }
