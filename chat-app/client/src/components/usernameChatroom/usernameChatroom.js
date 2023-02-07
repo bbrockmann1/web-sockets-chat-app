2b01fcc8708f1f0cc3c4c555ca4efde181faf418
Bo Brockmann <bbrockmann1@gmail.com>
2023-02-06 19:42:54 -0700

    controlled form

--- chat-app/client/src/components/usernameChatroom/usernameChatroom.js
+++ chat-app/client/src/components/usernameChatroom/usernameChatroom.js
@@ -1,14 +1,25 @@
-import './usernameChatroom.css' 
+import { useState } from 'react';
+import './usernameChatroom.css';
 
 function UsernameChatroom() {
+    const [username, setUsername] = useState('');
+    const [room, setRoom] = useState('');
 
     return(
         <div className='container'> 
             <h1>Select a Username and Create a Room</h1>
             <div className='field-container'>
                 <form>
-                    <input placeholder='Username'/>
-                    <input placeholder='Room Name'/>
+                    <input 
+                        placeholder='Username'
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                    <input 
+                        placeholder='Room Name'
+                        value={room}
+                        onChange={e => setRoom(e.target.value)}
+                    />
                     <button type='submit'>Submit</button>
                 </form>
             </div>
