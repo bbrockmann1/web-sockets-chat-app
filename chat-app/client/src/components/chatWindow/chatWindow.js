8bf60f86d7135b88a0f39a6e1413fec96954c63c
Bo Brockmann <bbrockmann1@gmail.com>
2023-02-10 10:09:15 -0700

    some css to chat window

--- chat-app/client/src/components/chatWindow/chatWindow.js
+++ chat-app/client/src/components/chatWindow/chatWindow.js
@@ -11,17 +11,19 @@ function ChatWindow() {
     };
 
     return(
-        <div > <h1>Chat</h1>
-            <div>
-                {/* Chat messages go here. */}
+        <div className='container-all'>
+            <div > <h1 className='chat-header'>Chat</h1>
+                <div className='chatContainer'>
+                    {/* Chat messages go here. */}
+                </div>
+                <div className='chatInputContainer'>
+                    <form className='chatInput' onSubmit={handleSubmit}>
+                        <input type='text' placeholder='Enter your message'/>  
+                        <input type='submit' value='Send'/>
+                    </form>
+                </div>
             </div>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <input type='text' placeholder='Enter your message'/>  
-                    <input type='submit' value='Send'/>
-                </form>
-            </div>
-        </div>
+        </div>    
     );
 };
 
